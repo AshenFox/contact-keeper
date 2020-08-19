04829f4cc4f5ba5946937ce67c26ebdb70123051
HoarFox <kavokinm@gmail.com>
2020-08-19 14:48:56 +0300

    Commit 14:48 19.08.2020

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import AlertContext from "../../context/alert/alertContext";
 import AuthContext from "../../context/auth/authContext";
 
-const Login = props => {
+const Login = (props) => {
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
 
@@ -23,12 +23,12 @@ const Login = props => {
 
   const [user, setUser] = useState({
     email: "",
-    password: ""
+    password: "",
   });
 
-  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
     if (email === "" || password === "") {
       setAlert("Please fill in all fields", "danger");
